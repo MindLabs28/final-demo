2d8e5369c1967d7fa9c18fb1bc4b7b49c271a3e3
Maitri Sharma <maitri.s@ciright.com>
2024-04-24 18:05:50 +0530

    changes

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,30 +1,74 @@
-import React from "react"
-import FadeIn from "../scroll components/FadeIn"
+import { motion } from "framer-motion"
+import { Tooltip, TooltipProps, styled, tooltipClasses } from "@mui/material"
+
+const StyledTooltip = styled(({ className, ...props }: TooltipProps) => (
+  <Tooltip
+    {...props}
+    classes={{ popper: className }}
+  />
+))(() => ({
+  [`& .${tooltipClasses.tooltip}`]: {
+    backgroundColor: "rgb(248 113 113 / 1) ",
+  },
+  [`& .${tooltipClasses.arrow}`]: {
+    color: "rgb(248 113 113 / 1)",
+  },
+}))
 
 const Card = ({
   src,
   title,
-  children,
+  className,
+  description,
 }: {
   src: string
   title: string
-  children: React.ReactNode
+  className?: string
+  description: string
 }) => {
   return (
-    <div className="rounded-xl flex gap-[15rem]">
-      <img
-        src={src}
-        alt="not-found"
-        className=" object-cover  rounded-lg  w-[12rem] aspect-[12/15]"
-      />
-      <FadeIn
-        from="top"
-        className="flex-1"
+    <StyledTooltip
+      title={
+        <p className="font-bold text-[0.8rem] capitalize">{description}</p>
+      }
+      arrow
+      placement="top"
+    >
+      <motion.div
+        className="w-[14rem] space-y-5 relative cursor-pointer"
+        initial={{ scale: 0 }}
+        whileInView={{
+          scale: 1,
+        }}
+        transition={{ delay: 0.1 }}
+        whileHover={"show"}
+        whileTap={"show"}
       >
-        <h3 className="text-purple-400 font-bold text-3xl">{title}</h3>
-        {children}
-      </FadeIn>
-    </div>
+        {/* <motion.div
+        className="bg-red-400 rounded-lg absolute top-[0%] left-[50%] translate-x-[-50%] translate-y-[-1.5rem] shadow-2xl px-5 font-bold max-w-[14rem] text-white"
+        initial={{ opacity: 0 }}
+        variants={{
+          show: {
+            opacity: 1,
+          },
+        }}
+      >
+        {description}
+        <div
+          style={{
+            clipPath: "path('m 0 0 c 10 12 4 12 15 0')",
+          }}
+          className="aspect-square h-[15px] bg-red-400 absolute top-[95%] left-[50%] translate-x-[-50%]"
+        />
+      </motion.div> */}
+
+        <img
+          src={src}
+          className={`w-full aspect-[10/14] object-cover object-top rounded-xl ${className}`}
+        />
+        <h3 className="text-white font-bold text-center ">{title}</h3>
+      </motion.div>
+    </StyledTooltip>
   )
 }
 
--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,11 +1,10 @@
 import Nav from "../Navbar/Nav"
 import SlideIn from "../scroll components/SlideIn"
 import Card from "../card/Card"
-import { GiCarWheel } from "react-icons/gi"
 
 const Home = () => {
   return (
-    <main className="pt-[72px]">
+    <main className="pt-[72px] bg-black">
       <Nav />
       <div className="bg-mainBg h-screen bg-no-repeat bg-cover bg-center  relative isolate flex flex-col">
         <div className="absolute inset-0 bg-[rgb(0,0,0,0.4)] z-[-1] " />
@@ -15,7 +14,7 @@ const Home = () => {
           className="space-y-5 pl-[2rem] md:pl-[7rem] relative top-[10%] sm:top-[7%] lg:top-[25%]"
         >
           <h3 className="text-[yellow] text-xl font-bold">
-            27 hours 7 days a week availability
+            24 hours 7 days a week availability
           </h3>
           <h2 className="text-white text-4xl font-bold">
             Big Or Small... We Do It All
@@ -32,27 +31,66 @@ const Home = () => {
         </SlideIn>
       </div>
       <section
-        className="bg-black text-primary md:px-[5rem]"
+        className=" text-primary md:px-[5rem]"
         id="services"
       >
         <SlideIn side="left">
-          <h2 className="font-bold text-4xl pt-5 pb-[2.5rem]">
-            What We Provide
-          </h2>
+          <h2 className="font-bold text-5xl pt-5 pb-[2.5rem]">Our Services:</h2>
         </SlideIn>
-        <div>
+        <div className=" flex flex-wrap gap-8 justify-center">
           <Card
             src="https://github.com/pathikcomp/images/blob/main/truck-2.jpg?raw=true"
-            title="Truck Wash"
-          >
-            <ul>
-              <li>
-                <GiCarWheel /> truck wash
-              </li>
-              <li>truck clean</li>
-            </ul>
-          </Card>
+            title="Fleet Trucks"
+            description="Washing Trucks And Trailers"
+          />
+          <Card
+            src="https://github.com/pathikcomp/images/blob/main/park.jpg?raw=true"
+            title="Gas Station Canopy & Car Wash"
+            description="Washing Car,Glass Windows,Canopy And Car Wash Area"
+          />
+          <Card
+            src="https://github.com/pathikcomp/images/blob/main/IMG-20240416-WA0000.jpg?raw=true"
+            title="Parking And UnderGround"
+            description="Washing Park Area Dirt Of UnderGround Area & Wash Parking Area"
+          />
+          <Card
+            src="https://github.com/pathikcomp/images/blob/main/store.jpg?raw=true"
+            title="Store Front"
+            className="object-[right,center]"
+            description="cleaning store font area of any store"
+          />
+          <Card
+            src="https://github.com/pathikcomp/images/blob/main/house-1.jpg?raw=true"
+            title="Residential Cleaning"
+            // className="object-[right,center]"
+            description="cleaning any residential buildings"
+          />
+          <Card
+            src="https://github.com/pathikcomp/images/blob/main/graffiti-removal-1.jpg?raw=true"
+            title="Graffiti Removal"
+            className="object-[25%,50%]"
+            description="removing spray paints from any wall"
+          />
+          <Card
+            src="https://github.com/pathikcomp/images/blob/main/water.jpeg?raw=true"
+            title="Hot Water Supply"
+            className="object-[70%,50%]"
+            description="we supply hot water through our trucks"
+          />
+          <Card
+            src="https://github.com/pathikcomp/images/blob/main/IMG-20240421-WA0008.jpg?raw=true"
+            title="Commercial Cleaning"
+            className="object-[0%,100%]"
+            description="cleaning any commercial spaces"
+          />
         </div>
+      </section>
+      <section className="mt-5 md:px-[5rem]">
+        <SlideIn side="left">
+          <h2 className="text-primary font-bold text-5xl pt-5 pb-[2.5rem]">
+            Contact Us:
+          </h2>
+        </SlideIn>
       </section>
     </main>
   )
